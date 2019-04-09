61686efc81e6287b02a428db86f3a2c252053488
SreejithNS <nssreejith229@gmail.com>
2019-04-09 15:48:43 +0000

    Two new pages for testing

---
+++ src/pages/YourOrders.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class YourOrders extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                Orders Page
+            </div>
+        )
+    }
+}
