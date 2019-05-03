cfc1d6e720958ffb0bd3844157b70601b38f11b7
Sreejith N <nssreejith229@gmail.com>
2019-05-03 13:41:48 +0000

    Firebase reducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,8 +8,9 @@ import thunk from "redux-thunk";
 import {compose} from "redux"
 import firebaseConfig from '../config/firebaseConfig';
 import {firestoreReducer} from "redux-firestore";
+import {firebaseReducer} from "react-redux-firebase"
 
-const store = createStore(combineReducers({user,ui,firestore:firestoreReducer}),{},
+const store = createStore(combineReducers({user,ui,firestore:firestoreReducer,firebase:firebaseReducer}),{},
     compose(
         applyMiddleware(logger,thunk.withExtraArgument({getFirebase,getFirestore})),
         reduxFirestore(firebaseConfig),
